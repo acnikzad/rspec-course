15554ec4e896410f03b283f95177745638795bdc
Aria Nikzad <a.c.nikzad@gmail.com>
2022-11-23 18:08:48 -0800

    added more _spec tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -7,15 +7,22 @@ class Card
     end
 end
 
-
 RSpec.describe Card do
-    it 'has a rank' do
-        card = Card.new('Ace', 'Spades')
+    let(:card) { Card.new('Ace', 'Spades') }
+
+    it 'has a rank and that rank can change' do
         expect(card.rank).to eq('Ace')
+        # card.rank = 'Queen'
+        # expect(card.rank).to eq('Queen')
     end
 
     it 'has a suit' do
-        card = Card.new('Ace', 'Spades')
         expect(card.suit).to eq('Spades')
+    end
+
+    it 'has a custom error message' do
+        card.suit = "Nonsense"
+        comparison = 'Spades'
+        expect(card.suit).to eq(comparison), "Hey, I expected #{comparison} but I got #{card.suit} instead"
     end
 end 
