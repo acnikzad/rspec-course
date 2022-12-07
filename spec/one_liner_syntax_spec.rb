e89c640a07118be06e0725a48120e30ba7dfbc58
Aria Nikzad <a.c.nikzad@gmail.com>
2022-12-07 13:55:41 -0800

    added shared_examples test

---
+++ spec/one_liner_syntax_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe 'shorhand syntax' do
+    subject{ 5 }
+
+
+    context 'describe classic syntax' do
+        it 'should equal 5' do
+            expect(subject).to eq(5)
+        end
+    end
+
+    context 'with one-liner syntax' do
+        it { is_expected.to eq(5) }
+    end
+
+end
